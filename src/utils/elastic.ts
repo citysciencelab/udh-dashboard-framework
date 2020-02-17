b209798d99c3a635cd8e5075001007afe5fb21d1
Jan Behrens <jan.behrens@hcu-hamburg.de>
2020-02-17 14:41:01 +0100

    nicer module format

--- src/utils/elastic.ts
+++ src/utils/elastic.ts
@@ -2,7 +2,7 @@ import Axios from 'axios';
 
 const baseUrl = 'https://test-geodienste.hamburg.de/udh_dashboard/_search';
 
-export default {
+const elastic = {
     /*
      * Get data from Elasticsearch endpoint "test_rangeless"
      * Example:
@@ -11,9 +11,9 @@ export default {
         "date": "2019-11",
         "category": "datasets"
      */
-    getRangeless: async (theme: string, org: string, date: string, category: string) => {
-        validateDate(date);
-        validateCategory(category);
+    async getRangeless(theme: string, org: string, date: string, category: string) {
+        elastic.validateDate(date);
+        elastic.validateCategory(category);
         const params = { theme, org, date, category };
         const source = JSON.stringify({ id: 'test_rangeless', params: params });
         const url = encodeURI(`${baseUrl}/template?source=${source}&source_content_type=application/json`);
@@ -32,34 +32,36 @@ export default {
         "top": 2,
         "interval": "year"
      */
-    getRangeful: async (theme: string, org: string, from: string, to: string, category: string, top: number, interval: string) => {
-        validateDate(from);
-        validateDate(to);
-        validateCategory(category);
-        validateInterval(interval);
+    async getRangeful(theme: string, org: string, from: string, to: string, category: string, top: number, interval: string) {
+        elastic.validateDate(from);
+        elastic.validateDate(to);
+        elastic.validateCategory(category);
+        elastic.validateInterval(interval);
         const params = { theme, org, from, to, category, top, interval };
         const source = JSON.stringify({ id: 'test_rangeful', params: params });
         const url = encodeURI(`${baseUrl}/template?source=${source}&source_content_type=application/json`);
         const response = await Axios.get(url);
         return response.data.aggregations;
+    },
+
+    validateDate(date: string) {
+        // it is trivial, but better than nothing
+        if (!date.match(/^[0-9]{4}-[0-9]{2}$/)) {
+            throw new Error('`Invalid date format. Must be YYYY-MM.');
+        }
+    },
+
+    validateCategory: (category: string) => {
+        if (['datasets', 'apps', 'downloads'].indexOf(category) === -1) {
+            throw new Error(`Invalid category. Must be 'datasets', 'apps' or 'downloads'.`);
+        }
+    },
+
+    validateInterval: (category: string) => {
+        if (['month', 'year'].indexOf(category) === -1) {
+            throw new Error(`Invalid interval. Must be 'month' or 'year'.`);
+        }
     }
 };
 
-function validateDate(date: string) {
-    // it is trivial, but better than nothing
-    if (!date.match(/^[0-9]{4}-[0-9]{2}$/)) {
-        throw new Error('`Invalid date format. Must be YYYY-MM.');
-    }
-}
-
-function validateCategory(category: string) {
-    if (['datasets', 'apps', 'downloads'].indexOf(category) === -1) {
-        throw new Error(`Invalid category. Must be 'datasets', 'apps' or 'downloads'.`);
-    }
-}
-
-function validateInterval(category: string) {
-    if (['month', 'year'].indexOf(category) === -1) {
-        throw new Error(`Invalid interval. Must be 'month' or 'year'.`);
-    }
-}
+export default elastic;
