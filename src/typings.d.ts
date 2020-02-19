864af7baff0da1eb0a102bc86a905f4c88f23a5d
a3rtgm-ds <daniel.schulz@hcu-hamburg.de>
2020-02-19 16:43:43 +0100

    masterportal-api typings

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -108,6 +108,62 @@ interface IUtils {
   }
 }
 
+declare module 'masterportalAPI' {
+  import { Map, Feature, View } from 'ol'
+  import { Coordinate } from 'ol/coordinate';
+  import { Layer } from 'ol/layer'
+  import { Vector as VectorLayer } from 'ol/layer'
+  import { Vector, TileWMS, ImageWMS } from 'ol/source'
+  import { StyleLike } from 'ol/style/Style'
+
+  const mpapi: {
+    createMap(config?: object, params?: object): Map
+    createMapView(config: object): View
+    wms: {
+      generateSessionId(): number
+      makeParams(rawLayer: object): object
+      createLayerSource(rawLayer: object): TileWMS | ImageWMS
+      createLayer(rawLayer: object): Layer
+      updateSource(layer: Layer): number
+      getGfiURL(layer: Layer, map: Map, coordinate: Coordinate): string | undefined
+    }
+    geojson: {
+      setCustomStyles(styles: object): void
+      createLayerSource(rawLayer: object, map: Map): Vector
+      createLayer(rawLayer: object, params?: object): VectorLayer
+      updateSource(layer: VectorLayer): void
+      setFeatureStyle(features: Feature[], featureStyle: StyleLike): void
+      hideAllFeatures(layer: Layer): void
+      showAllFeatures(layer: Layer): void
+      showFeaturesById(layer: Layer, featureIdList: string[]): void
+    }
+    layerLib: {
+      isLayerVisibleInResolution(layer: Layer, params: object): boolean
+      getLegendURLs (rawLayer: object): string[]
+    }
+    setBackgroundImage(config?: object): void
+    setGazetteerUrl(url: string): void
+    rawLayerList: {
+      initializeLayerList(layerConf?: object, callback?: unknown): void
+      getLayerWhere(searchAttributes: object): object
+      getLayerList(): object[] | null
+      getDisplayNamesOfFeatureAttributes(layerId: string, featureAttribute: string): object | string | null
+    }
+    search(searchstring: string, params: object): Promise<object[]>
+    crs: {
+      registerProjections(namedProjections?: string[]): void
+      getProjection(name: string): object | undefined
+      getProjections(): object[]
+      getMapProjection(map: Map): string
+      getProj4Projection(projection: string | object): object | undefined
+      transform(sourceProjection: string | object, targetProjection: string | object, point: number[]): number[] | undefined
+      transformToMapProjection(map: Map, sourceProjection: string | object, point: number[]): number[] | undefined
+      transformFromMapProjection(map: Map, targetProjection: string | object, point: number[]): number[] | undefined
+    }
+  }
+  export = mpapi;
+}
+
 interface DateRangeSlider {
   defaultValue: number[];
   step: number;
