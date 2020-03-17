1bd7f708bd4e5f3d975e9a512d72e9b9a1debcd8
Jan Behrens <jan.behrens@hcu-hamburg.de>
2020-03-17 12:28:12 +0100

    remove unnecessary imports

--- src/components/charts/PieChart.vue
+++ src/components/charts/PieChart.vue
@@ -14,7 +14,6 @@
 import { Component } from 'vue-property-decorator';
 import * as d3 from 'd3';
 import _d3tip from 'd3-tip';
-import { Tooltip } from 'd3/types/d3';  // eslint-disable-line no-unused-vars
 import AbstractChart from './AbstractChart.vue';
 
 const d3tip = _d3tip as () => Tooltip;
