9ae55238df15f826d6ef756676ab05dd4d73b064
Till Degkwitz <till.degkwitz@hcu-hamburg.de>
2020-03-25 15:48:29 +0100

    kpi visitors no static month in query

--- src/store/udpc.module.ts
+++ src/store/udpc.module.ts
@@ -82,10 +82,10 @@ const udpcModule: Module<UDPCState, RootState> = {
             }]);
         },
         fetchVisitorsKPI: async (context) => {
-            const aggregations = await elastic.getRangeful('', '', '2020-02', '2020-03', 'datasets', undefined, 'month', undefined);
-            console.log(aggregations)
+            const today = new Date();
+            const month = `${today.getFullYear()}-${today.getMonth() < 10 ? '0' : ''}${today.getMonth()}`;
+            const aggregations = await elastic.getRangeful('', '', month, month, 'datasets', undefined, 'month', undefined);
             context.commit('SET_FILTERED_DATA', ['visitorsKPI', aggregations.total_entities_and_hits.buckets[0].total_hits.value]);
-
         },
         applyFilter: (context, [id, accessor]) => {
             const filterFunction = (item: Datum) => context.state.filters[id].indexOf(item[accessor]) > -1;
