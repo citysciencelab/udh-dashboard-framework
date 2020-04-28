452a33a7dd188148bf5107356efb6f398a308411
Till Degkwitz <till.degkwitz@hcu-hamburg.de>
2020-04-28 10:00:40 +0200

    attributes with ''

--- src/components/charts/d3/TreeMapChartD3.vue
+++ src/components/charts/d3/TreeMapChartD3.vue
@@ -14,9 +14,9 @@ import {Tooltip} from "d3/types/d3";
 
   // eslint-disable-next-line no-unused-vars
   import {Tooltip} from 'd3/types/d3';
-  import _d3tip from "d3-tip";
-  import Color from "color";
-  import $ from "jquery";
+  import _d3tip from 'd3-tip';
+  import Color from 'color';
+  import $ from 'jquery';
 
   const d3tip = _d3tip as () => Tooltip;
 
@@ -64,59 +64,59 @@ import {Tooltip} from "d3/types/d3";
 
       // Create tooltip
       const tip = d3tip()
-       .attr("class", "d3-tip")
+       .attr('class', 'd3-tip')
        .offset([-8, 0])
        .html(function (d: Datum) {
-         return d['id'] + ": " + d['value'];
+         return d['id'] + ': ' + d['value'];
        });
       svg.call(tip);
 
-      var cell = svg.selectAll("g")
+      var cell = svg.selectAll('g')
        .data(root.leaves())
-       .enter().append("g")
+       .enter().append('g')
        .attr('transform', 'translate(' + xOffset + ',' + yOffset + ')');
 
       // Create the treemap elements
-      cell.append("rect")
-       .attr("id", function (d) {
+      cell.append('rect')
+       .attr('id', function (d) {
          return d.data.id;
        })
-       .attr("width", function (d) {
+       .attr('width', function (d) {
          return d.x1 - d.x0;
        })
-       .attr("height", function (d) {
+       .attr('height', function (d) {
          return d.y1 - d.y0;
        })
        .attr('x', d => d.x0)
        .attr('y', d => d.y0)
-       .attr("fill", function (d) {
+       .attr('fill', function (d) {
          return d.data.color ? d.data.color : 'slateblue';
        })
        .on('mouseover', tip.show)
        .on('mouseenter', function (d) {
-         d3.select(this).attr("fill", Color(d.data.color ? d.data.color : 'slateblue').alpha(0.9).hex());
+         d3.select(this).attr('fill', Color(d.data.color ? d.data.color : 'slateblue').alpha(0.9).hex());
        })
        .on('mouseout', function (d) {
          tip.hide();
-         d3.select(this).attr("fill", d.data.color ? d.data.color : 'slateblue');
+         d3.select(this).attr('fill', d.data.color ? d.data.color : 'slateblue');
        })
-       .on("click", this.clickChartElement);
+       .on('click', this.clickChartElement);
 
       const textMarginH = 12;
       const textMarginW = 5;
 
       // Create the labels
-      cell.append("text")
-       .attr("width", function (d) {
+      cell.append('text')
+       .attr('width', function (d) {
          return d.x1 - d.x0;
        })
        .attr('x', d => d.x0 + textMarginW)
        .attr('y', d => d.y0 + textMarginH)
        .attr('transform', 'translate(' + xOffset + ',' + yOffset + ')')
        .attr('class', 'treemap-label')
-       .attr("font-size", "12px")
-       .attr("fill", "white")
-       .on("click", this.clickChartElement)
+       .attr('font-size', '12px')
+       .attr('fill', 'white')
+       .on('click', this.clickChartElement)
        .text(function (d): string {
          return d['id'] + '';
        });
@@ -136,7 +136,7 @@ import {Tooltip} from "d3/types/d3";
       }
     }
 
-    clickChartElement(d) {
+    clickChartElement(d: Node) {
       this.$emit('click', d);
     }
   }
