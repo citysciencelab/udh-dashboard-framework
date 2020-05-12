93fb587e2e3153e22092a674051d99602f456902
a3rtgm-ds <daniel.schulz@hcu-hamburg.de>
2020-05-12 09:29:50 +0200

    add - parseLinkFromString

--- src/components/DidYouKnowDataList.vue
+++ src/components/DidYouKnowDataList.vue
@@ -3,13 +3,14 @@
     <li v-for="(el, i) in inputData.items"
         :key="i">
       <h6>
-        {{ el.label }}
-        <!-- <div v-html="el"></div> -->
-        <a v-if="el.link"
-           :href="linkUrl(el.link)"
-           target="_blank">
-          <md-icon v-if="linkUrl">launch</md-icon>
-        </a>
+        <!-- eslint-disable-next-line vue/no-v-html -->
+        <div v-html="utils.string.parseLinkFromString(el.label)">
+          <a v-if="el.link"
+             :href="linkUrl(el.link)"
+             target="_blank">
+            <md-icon v-if="linkUrl">launch</md-icon>
+          </a>
+        </div>
       </h6>
     </li>
   </ol>
@@ -17,19 +18,17 @@
 
 <script lang="ts">
 import { Component, Vue, Prop } from 'vue-property-decorator';
+import Utils from '@/utils/utils'
 
 @Component({})
 export default class DidYouKnowDataList extends Vue {
     @Prop() inputData!: DidYouKnowData;
     @Prop() linkPrefix!: string;
 
+    utils = new Utils();
+
     linkUrl(path: string): string | null {
         return this.linkPrefix ? this.linkPrefix + path : path;
-    }
-
-    parseLinks(str: string) {
-        // return str.indexOf('://') > -1 ? str.split()
-        return str;
     }
 }
 </script>
