e0b9d655ab9167bba0ca11748aa594c76034e91a
Nicole Schubbe <nicole.schubbe@gv.hamburg.de>
2020-06-25 06:02:26 +0000

    messages.udpc.module.ts edited online with Bitbucket Ich bin fertig, Grant schaut nochmal drüber.

--- src/messages/messages.udpc.module.ts
+++ src/messages/messages.udpc.module.ts
@@ -63,16 +63,16 @@ export const messages = {
              'Unter <b>Apps</b> wird die Gesamtzahl von allen im Internet und Intranet erreichbaren Anwendungen des LGV in einem monatlichen oder jährigen Zeitverlauf aufgeführt.  \n' +
              '<b>Sensordatensätze</b> beschreiben alle Datensätze, die mittels Sensoren in bzw. nahezu in Echtzeit erstellt und aktualisiert werden. Sie sind eine Teilmenge der Datensätze.\n' +
              'Quelle: CSW-Schnittstelle des HMDK (Hamburger Metadatenkatalog)',
-            tooltipMap: 'Ein Klick auf den Datensatznamen in der Rubrik „Neuste Datensätze“ führt zu einer Vorschau des Datensatzes in der Karte.\n' +
-             'Neuste Datensätze sind die 10 Datensätze, die im Hamburger Metadatenkatalog zuletzt beschrieben wurden (create date) und mit produktiven Diensten (WFS/WMS) gekoppelt sind. \n' +
-             'Ein Klick auf den Vergrößerungsbutton stoppt den Durchlauf der „Neusten Datensätze“ und zeigt den aktuellen Datensatz in der vergrößerten Karte an.\n' +
-             'Die Metadatensatzbeschreibungen von im Internet veröffentlichten Datensätzen finden Sie unter <a href="https://metaver.de/" target="_blank">metaver.de</a>.\n' +
+            tooltipMap:  'Ein Klick auf den Datensatznamen in der Rubrik „Neueste Datensätze“ führt zu einer Vorschau des Datensatzes in der Karte.\n' +
+             'Neueste Datensätze sind die 10 Datensätze, die im Hamburger Metadatenkatalog zuletzt beschrieben wurden (create date) und mit produktiven Diensten (WFS/WMS) gekoppelt sind. \n' +
+             'Ein Klick auf den Vollbildbutton stoppt den Durchlauf der „Neuesten Datensätze“ und zeigt den aktuellen Datensatz in der vergrößerten Karte an.\n' +
+             'Mit einem Klick auf den Namen des Datensatzes in der Karte gelangen Sie zu den Metadatensatzbeschreibungen von im Internet veröffentlichten Datensätzen <a href="https://metaver.de/" target="_blank">metaver.de</a>.\n' +
              'Quelle: CSW-Schnittstelle des HMDK (Hamburger Metadatenkatalog)',
             tooltipTop_datasets: '<p>Zeigt Datensätze, Anwendungen (Apps) mit den meisten Zugriffen oder die Downloads mit den meisten Hits an. \n' +
              'Mit einem Klick auf den jeweiligen Balken gelangen Sie zur Metadatensatzbeschreibung in <a href="https://metaver.de" target="_blank">MetaVer</a> \n' +
              '<h6>Die Top 10 Datensätze sind:</h6>' +
              'PLATZHALTER\n'+
-             '<p>Ein <b>Datensatz</b> ist eine, vom Dateneigentümer definierter, inhaltlich und/oder räumlich zusammengehöriger Gruppe von Daten in einer Datei. Bei den Datensätzen werden nur die Fachdaten also nur Datensätze, die keine Hintergrundkarten sind, einbezogen. Der Zusatz FHHNET zeigt an, dass der Datensatz nur im Intranet der FHH verfügbar ist, daher gibt es auch keinen Eintrag in MetaVer. \n' +
+             '<p>Ein <b>Datensatz</b> ist eine, vom Dateneigentümer definierte, inhaltlich und/oder räumlich zusammengehörige Gruppe von Daten in einer Datei. Bei den Datensätzen werden nur die Fachdaten also nur Datensätze, die keine Hintergrundkarten sind, einbezogen. Der Zusatz FHHNET zeigt an, dass der Datensatz nur im Intranet der FHH verfügbar ist, daher gibt es auch keinen Eintrag in MetaVer. \n' +
              'Bei den <b>Apps</b> werden die Anwendungen des LGV berücksichtigt, die im Internet und über das Intranet verfügbar sind.  \n' +
              'Bei den <b>Downloads</b> werden alle Daten berücksichtigt, die über den Downloadserver der UDP heruntergeladen wurden. \n\n' +
              'Quelle: Auswertung der awstats</p>',
@@ -92,14 +92,15 @@ export const messages = {
              'Bei den <b>Apps</b> werden die Anwendungen des LGV berücksichtigt, die im Internet und über das Intranet verfügbar sind.  \n' +
              'Bei den <b>Downloads</b> werden alle Daten berücksichtigt, die über den Downloadserver der UDP heruntergeladen wurden. \n\n' +
              'Quelle: Auswertung der awstats</p>',
-            tooltipDownloads: 'Es wird die Anzahl der monatlichen bzw. jährlichen Downloads in einem historischen Zeitverlauf angezeigt.\n' +
+            tooltipDownloads: 'Es wird die Anzahl der monatlichen bzw. jährlichen Downloads von Dokumenten (z.B. PDF, JPEG, usw.) vom Downloadserver der UDP aufsummiert und im Zeitverlauf angezeigt.\n' +
              'Quelle: Auswertung der awstats',
-            tooltipAccessData: 'Hier werden die monatlichen bzw. jährlichen Zugriffe auf die Fachdaten im Internet und Intranet der FHH mit Historie angegeben. Fachdaten sind alle Datensätze, die keine Hintergrundkarten sind.\n' +
+            tooltipAccessData: 'Hier werden die monatlichen bzw. jährlichen Zugriffe auf die Fachdaten im Internet und Intranet der FHH mit Historie angegeben. Fachdaten sind alle Datensätze, die keine Hintergrundkarten sind. \n' + 
+            'Der starke Anstieg der Zugriffe von 2018 zu 2019 ist damit zu erklären, dass erst seit November 2018 alle Zugriffe automatisiert ermittelt werden. Vorher war dies nur teilweise möglich.\n' +
              'Quelle: Auswertung der awstats',
-            tooltipAccessApps: 'Es werden die monatlichen bzw. jährlichen Gesamtzugriffe auf die Internet- und Intranet-Anwendungen (Apps) des LGV in einem historischen Zeitverlauf angezeigt.\n' +
+            tooltipAccessApps: 'Es werden die monatlichen bzw. jährlichen Gesamtzugriffe auf die Internet- und Intranet-Anwendungen (Apps) des Landesbetriebes Geoinformation und Vermessung (LGV) im Zeitverlauf angezeigt.\n' +
              'Welche Apps dazu gehören erfahren Sie in den Metadatenkatalogen:\n' +
              'Link für Internet-Apps: <a href="https://metaver.de/freitextsuche?action=doSearch&q=iplugs%3A%22%2Fingrid-group%3Aige-iplug-HH%22+t01_object.obj_class%3A6&grouping=domain" target="_blank">metaver.de/freitextsuche</a> \n' +
-             'Link für Intranet-Apps: <a href="http://hmdk.fhhnet.stadt.hamburg.de/freitextsuche?action=doSearch&q=iplugs%3A%22%2Fingrid-group%3Aige-iplug-HH%22+t01_object.obj_class%3A6&grouping=domain" target="_blank">hmdk.fhhnet.stadt.hamburg.de</a> \n' +
+             'Link für Intranet-Apps (FHHNET): <a href="http://hmdk.fhhnet.stadt.hamburg.de/freitextsuche?action=doSearch&q=iplugs%3A%22%2Fingrid-group%3Aige-iplug-HH%22+t01_object.obj_class%3A6&grouping=domain" target="_blank">hmdk.fhhnet.stadt.hamburg.de</a> \n' +
              'Quelle: Auswertung der awstats',
             privacy: 'Datenschutz',
             legal: 'Impressum',
@@ -170,43 +171,45 @@ export const messages = {
              'The tab <b>Apps</b> shows the total number of all LGV applications accessible on the Internet and Intrane, listed in a monthly or yearly timeline.  \n' +
              '<b>Sensor datasets</b> describe all datasets that are created and updated by sensors in real time. They are a subset of datasets. '\n'.
              'Source: CSW-API of HMDK (Hamburger Metadatenkatalog)',
-            tooltipMap: 'Ein Klick auf den Datensatznamen in der Rubrik „Neuste Datensätze“ führt zu einer Vorschau des Datensatzes in der Karte.\n' +
-             'Neuste Datensätze sind die 10 Datensätze, die im Hamburger Metadatenkatalog zuletzt beschrieben wurden (create date) und mit produktiven Diensten (WFS/WMS) gekoppelt sind. \n' +
-             'Ein Klick auf den Vergrößerungsbutton stoppt den Durchlauf der „Neusten Datensätze“ und zeigt den aktuellen Datensatz in der vergrößerten Karte an.\n' +
-             'Die Metadatensatzbeschreibungen von im Internet veröffentlichten Datensätzen finden Sie unter <a href="https://metaver.de/" target="_blank">metaver.de</a>.\n' +
-             'Quelle: Metadaten der Datensätze (Hamburger Metadatenkatalog – HMDK)',
-            tooltipTop_datasets: '<p>Zeigt Datensätze, Anwendungen (Apps) mit den meisten Zugriffen oder die Downloads mit den meisten Hits an. \n' +
-             'Mit einem Klick auf den jeweiligen Balken gelangen Sie zur Metadatensatzbeschreibung in <a href="https://metaver.de" target="_blank">MetaVer</a> \n' +
-             '<h6>Die Top 10 Datensätze sind:</h6>' +
+            tooltipMap: 'Clicking on the record name in the "Recent records" section will display a preview of the record in the map.\n' +
+             'Newest records are the 10 datasets which were last described in the Hamburg metadata catalogue (create date) and are linked to productive services (WFS/WMS). \n' +
+             'Clicking on the full screen button stops the run of the "Newest datasets" and shows the current data set in the enlarged map.\n' +
+             'By clicking on the name of the dataset in the map you can access the metadata descriptions of datasets published on the Internet <a href="https://metaver.de/" target="_blank">metaver.de</a>.\n' +
+             'Source: CSW interface of the HMDK (Hamburg Metadata Catalogue)',
+            tooltipTop_datasets: '<p>Shows datasets, applications (apps) with the most accesses or the downloads with the most hits. \n' +
+             'By clicking on the respective bar, you can access the metadata description in <a href="https://metaver.de" target="_blank">MetaVer</a> \n' +
+             '<h6>The top 10 datasets are:</h6>' +
              'PLATZHALTER\n'+
-             '<p>Ein <b>Datensatz</b> ist eine, vom Dateneigentümer definierter, inhaltlich und/oder räumlich zusammengehöriger Gruppe von Daten in einer Datei. Bei den Datensätzen werden nur die Fachdaten also nur Datensätze, die keine Hintergrundkarten sind, einbezogen. Der Zusatz FHHNET zeigt an, dass der Datensatz nur im Intranet der FHH verfügbar ist, daher gibt es auch keinen Eintrag in MetaVer. \n' +
-             'Bei den <b>Apps</b> werden die Anwendungen des LGV berücksichtigt, die im Internet und über das Intranet verfügbar sind.  \n' +
-             'Bei den <b>Downloads</b> werden alle Daten berücksichtigt, die über den Downloadserver der UDP heruntergeladen wurden. \n\n' +
+             '<p>a <b>dataset</b> is a group of data in a file, defined by the data owner, which are related in content and/or location. In the case of datasets, only the specialist data is included, i.e. only datasets that are not background maps. FHHNET  indicates that the dataset is only available on the FHH intranet, so there is no entry in MetaVer. \n' +
+             'For the <b>Apps</b> the applications of the LGV are considered, which are available on the Internet and via the Intranet.  \n' +
+             'For the <b>Downloads</b>, all data downloaded via the download server of the UDP is taken into account. \n\n' +
              'Source: Analysis of awstats</p>',
-            tooltipTop_apps: '<p>Zeigt Datensätze, Anwendungen (Apps) mit den meisten Zugriffen oder die Downloads mit den meisten Hits an. \n' +
-             'Mit einem Klick auf den jeweiligen Balken gelangen Sie zur Metadatensatzbeschreibung in <a href="https://metaver.de" target="_blank">MetaVer</a> \n' +
-             '<h6>Die Top 10 Apps sind:</h6>' +
+            tooltipTop_apps: '<p>Shows datasets, applications (apps) with the most accesses or the downloads with the most hits. \n' +
+             'By clicking on the respective bar, you can access the metadata description in <a href="https://metaver.de" target="_blank">MetaVer</a> \n' +
+             '<h6>The top 10 Apps are:</h6>' +
              'PLATZHALTER\n'+
-             '<p>Ein <b>Datensatz</b> ist eine, vom Dateneigentümer definierter, inhaltlich und/oder räumlich zusammengehöriger Gruppe von Daten in einer Datei. Bei den Datensätzen werden nur die Fachdaten also nur Datensätze, die keine Hintergrundkarten sind, einbezogen. Der Zusatz FHHNET zeigt an, dass der Datensatz nur im Intranet der FHH verfügbar ist, daher gibt es auch keinen Eintrag in MetaVer. \n' +
-             'Bei den <b>Apps</b> werden die Anwendungen des LGV berücksichtigt, die im Internet und über das Intranet verfügbar sind.  \n' +
-             'Bei den <b>Downloads</b> werden alle Daten berücksichtigt, die über den Downloadserver der UDP heruntergeladen wurden. \n\n' +
+             '<p>a <b>dataset</b> is a group of data in a file, defined by the data owner, which are related in content and/or location. In the case of datasets, only the specialist data is included, i.e. only datasets that are not background maps. FHHNET  indicates that the dataset is only available on the FHH intranet, so there is no entry in MetaVer. \n' +
+             'For the <b>Apps</b> the applications of the LGV are considered, which are available on the Internet and via the Intranet.  \n' +
+             'For the <b>Downloads</b>, all data downloaded via the download server of the UDP is taken into account. \n\n' +
              'Source: Analysis of awstats</p>',
-            tooltipTop_downloads: '<p>Zeigt Datensätze, Anwendungen (Apps) mit den meisten Zugriffen oder die Downloads mit den meisten Hits an. \n' +
-             'Mit einem Klick auf den jeweiligen Balken gelangen Sie zur Metadatensatzbeschreibung in <a href="https://metaver.de" target="_blank">MetaVer</a> \n' +
-             '<h6>Die Top 10 Downloads sind:</h6>' +
+            tooltipTop_downloads: '<p>Shows datasets, applications (apps) with the most accesses or the downloads with the most hits. \n' +
+             'By clicking on the respective bar, you can access the metadata description in <a href="https://metaver.de" target="_blank">MetaVer</a> \n' +
+             '<h6>The top 10 Downloads are:</h6>' +
              'PLATZHALTER\n'+
-             '<p>Ein <b>Datensatz</b> ist eine, vom Dateneigentümer definierter, inhaltlich und/oder räumlich zusammengehöriger Gruppe von Daten in einer Datei. Bei den Datensätzen werden nur die Fachdaten also nur Datensätze, die keine Hintergrundkarten sind, einbezogen. Der Zusatz FHHNET zeigt an, dass der Datensatz nur im Intranet der FHH verfügbar ist, daher gibt es auch keinen Eintrag in MetaVer. \n' +
-             'Bei den <b>Apps</b> werden die Anwendungen des LGV berücksichtigt, die im Internet und über das Intranet verfügbar sind.  \n' +
-             'Bei den <b>Downloads</b> werden alle Daten berücksichtigt, die über den Downloadserver der UDP heruntergeladen wurden. \n\n' +
+             '<p>a <b>dataset</b> is a group of data in a file, defined by the data owner, which are related in content and/or location. In the case of datasets, only the specialist data is included, i.e. only datasets that are not background maps. FHHNET  indicates that the dataset is only available on the FHH intranet, so there is no entry in MetaVer. \n' +
+             'For the <b>Apps</b> the applications of the LGV are considered, which are available on the Internet and via the Intranet.  \n' +
+             'For the <b>Downloads</b>, all data downloaded via the download server of the UDP is taken into account. \n\n' +
              'Source: Analysis of awstats</p>',
-            tooltipDownloads: 'Es wird die Anzahl der monatlichen bzw. jährlichen Downloads in einem historischen Zeitverlauf angezeigt.\n' +
+            tooltipDownloads: ' The number of monthly or annual downloads of documents (e.g. PDF, JPEG, etc.) from the UDP download server is summed up and displayed over time.\n' +
              'Source: Analysis of awstats',
-            tooltipAccessData: 'Hier werden die monatlichen bzw. jährlichen Zugriffe auf die Fachdaten im Internet und Intranet der FHH mit Historie angegeben. Fachdaten sind alle Datensätze, die keine Hintergrundkarten sind.\n' +
+            tooltipAccessData: 'Here the monthly and annual accesses to the specalist data on the Internet and Intranet of the FHH are indicated with history.\n' +
+            'Specialist data are all datasets that are not background maps. \n' + 
+            'The strong increase in the number of hits from 2018 to 2019 is explained by the fact that only since November 2018 all hits have been determined automatically. Before this was only partially possible.\n' +
              'Source: Analysis of awstats',
-            tooltipAccessApps: 'Es werden die monatlichen bzw. jährlichen Gesamtzugriffe auf die Internet- und Intranet-Anwendungen (Apps) des LGV in einem historischen Zeitverlauf angezeigt.\n' +
-             'Welche Apps dazu gehören erfahren Sie in den Metadatenkatalogen:\n' +
-             'Link für Internet-Apps: <a href="https://metaver.de/freitextsuche?action=doSearch&q=iplugs%3A%22%2Fingrid-group%3Aige-iplug-HH%22+t01_object.obj_class%3A6&grouping=domain" target="_blank">metaver.de/freitextsuche</a> \n' +
-             'Link für Intranet-Apps: <a href="http://hmdk.fhhnet.stadt.hamburg.de/freitextsuche?action=doSearch&q=iplugs%3A%22%2Fingrid-group%3Aige-iplug-HH%22+t01_object.obj_class%3A6&grouping=domain" target="_blank">hmdk.fhhnet.stadt.hamburg.de</a> \n' +
+            tooltipAccessApps: 'The monthly or annual total accesses to the Internet and Intranet applications (Apps)of the Agency for Geoinformation and Surveying (LGV) are displayed over time.\n' +
+             'You can find out which apps are included in the metadata catalogues:\n' +
+             'Link for Internet Apps: <a href="https://metaver.de/freitextsuche?action=doSearch&q=iplugs%3A%22%2Fingrid-group%3Aige-iplug-HH%22+t01_object.obj_class%3A6&grouping=domain" target="_blank">metaver.de/freitextsuche</a> \n' +
+             'Link for Intranet-Apps (FHHNET): <a href="http://hmdk.fhhnet.stadt.hamburg.de/freitextsuche?action=doSearch&q=iplugs%3A%22%2Fingrid-group%3Aige-iplug-HH%22+t01_object.obj_class%3A6&grouping=domain" target="_blank">hmdk.fhhnet.stadt.hamburg.de</a> \n' +
              'Source: Analysis of awstats',
             privacy: 'Data privacy',
             legal: 'Legal notice',
